12e8b4f5c8786f02ab8186d18fba2353d7579cee
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2025-08-04 08:56:11 -0400

    feat: fetch token account

---
+++ .changeset/beige-hornets-rest.md
@@ -0,0 +1,5 @@
+---
+"gill": minor
+---
+
+add `fetchTokenAccount` function
--- packages/gill/src/programs/token/index.ts
+++ packages/gill/src/programs/token/index.ts
@@ -1,4 +1,5 @@
 export * from "./addresses";
+export * from "./fetch-token-account";
 export * from "./instructions";
 export * from "./reexports";
 export * from "./transactions";
